0382f5fc83a6b0cc8379753f5f5529293311cd97
DanielOsuntoyinbo <dosuntoyinbo@gmail.com>
2025-05-13 13:52:40 +0100

    Initial commit

---
+++ functions.py
@@ -0,0 +1,25 @@
+FILEPATH = 'todos.txt'
+
+
+def get_todos(filepath=FILEPATH):
+    """
+    Read a text file and return the list of to-do items
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """
+    Write to-do items  list in a txt file
+    """
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+# print(__name__)
+# print("I am outside")
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
